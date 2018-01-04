2c21890495dd6d1a3a16b9b94576361517b618dc
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2018-01-04 15:11:08 -0600

    update styling

--- qabot.js
+++ qabot.js
@@ -9,23 +9,22 @@ class QAbot {
             elements: [...document.querySelectorAll(this.selectors.component)],
             overlays: []
         }
-
         this.buildOverlays();
-
-        console.log(this.components);
     }
 
     show() {
-        this.components.overlays.forEach(overlay => document.body.appendChild(overlay));
-        console.log('show11');
+        setTimeout(_=> {
+            this.components.overlays.forEach(overlay => overlay.style.opacity = 1);
+        }, 0);
     }
 
     hide() {
-        console.log('hide');
+        this.components.overlays.forEach(overlay => overlay.style.opacity = 0);
     }
 
     buildOverlays() {
         this.components.elements.forEach(elm => this.components.overlays.push(this.singleOverlay(elm)));
+        this.components.overlays.forEach(overlay => document.body.appendChild(overlay));
     }
 
     singleOverlay(elm) {
@@ -34,11 +33,15 @@ class QAbot {
             overlay = document.createElement('div');
 
         overlay.setAttribute('style', `
+            background: rgba(0,0,0,.05);
             border: 1px solid red;
-            height: ${params.height}px;
             left: ${params.left}px;
+            min-height: ${params.height}px;
+            opacity: 0;
+            pointer-events: none;
             position: absolute;
             top: ${params.top + correction}px;
+            transition: all .4s;
             width: ${params.width}px;
             z-index: 999;
         `);
